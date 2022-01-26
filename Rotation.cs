2cd4db34e27900810b9c1267ee3a9b07e6ac86a8
Zworis <86747548+Zworis@users.noreply.github.com>
2022-01-26 11:32:22 +0100

    Add files via upload

---
+++ Rotation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    float rotatie = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //GameObject.Find("Cube (8)").GetComponent("Transform").Rotation = (0f,0f,rotatie);
+        transform.Rotate(rotatie,0f,0f);
+    }
+}
---
+++ Rotation2.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation2 : MonoBehaviour
+{
+    float rotatie = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(-rotatie,0f,0f);
+    }
+}
