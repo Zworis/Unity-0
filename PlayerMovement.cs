8caa47bcb8e57fff2687600751b5efc5f694addd
Zworis <86747548+Zworis@users.noreply.github.com>
2022-01-26 11:31:37 +0100

    Add files via upload

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -6,6 +6,8 @@ public class PlayerMovement : MonoBehaviour
 {
     Rigidbody m_Rigidbody;
     public float m_Thrust;
+    public float jumpForce;
+    bool allow = true;
     public float max_Speed;
     public int score;
     string tekst;
@@ -16,7 +18,7 @@ public class PlayerMovement : MonoBehaviour
         m_Rigidbody = GetComponent<Rigidbody>();
     }
 
-    void Update()
+    void FixedUpdate()
     {
         if (Input.GetKey("w") && GetComponent<Rigidbody>().velocity.z < max_Speed)
         {
@@ -34,9 +36,14 @@ public class PlayerMovement : MonoBehaviour
         {
             GetComponent<Rigidbody>().AddForce(new Vector3(m_Thrust ,0, 0), ForceMode.Impulse);
         }
+        if (Input.GetKeyDown("space") && allow == true){
+            GetComponent<Rigidbody>().AddForce(new Vector3(0 ,jumpForce, 0), ForceMode.Impulse);
+            allow = false;
+        }
     }
     void OnCollisionEnter(Collision collision)
     {
+        allow = true;
         if(collision.gameObject.tag == "Points")
         {
             //This is example
@@ -46,6 +53,9 @@ public class PlayerMovement : MonoBehaviour
             Destroy(collision.gameObject);
             //code your thing here
             if(score == 7){
+                transform.position = new Vector3(16f,750f,1039f);
+            }
+            else if(score == 8){
                 Application.Quit();
             }
 
@@ -54,10 +64,15 @@ public class PlayerMovement : MonoBehaviour
     void OnTriggerEnter(Collider other){
         max_Speed = 20;
         m_Thrust = 0.25f;
-        print("Hello Peter");
+        if(other.gameObject.name == "Checkpoint"){
+            transform.position = new Vector3(16f,750f,1039f);
+        }
+        else if(other.gameObject.name == "Checkpoint2"){
+            transform.position = new Vector3(0f,0.375f,-7.38f);
+        }
     }
     void OnTriggerExit(Collider other){
-        max_Speed = 3;
-        m_Thrust = 0.14f;
+        max_Speed = 5;
+        m_Thrust = 0.18f;
     }
 }
